f79465f38bb2b55b2f4ffe00a0ab9ee844d8da13
michel <michel@localhost.localdomain>
2017-05-15 16:11:09 +0200

    création du dossier sources_temp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,8 +1,15 @@
-#include "application.hpp"
+#include <iostream>
+#include "fenetreSDL.hpp"
+#include "input.hpp"
 
 int main( /*int argc, char* args[] */)
 {
-    Application application;
-    application.applicationLoop();
+    Fenetre fen;
+    Input inp;
+    while(!inp.getInputState(QUITTER)){
+    	inp.misAJour();
+    	if(inp.getInputState(GAUCHE))
+    		std::cout<<"gauche"<<std::endl;
+    }
     return 0;
 }
